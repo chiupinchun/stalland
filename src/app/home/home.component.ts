0b56cbe214b93d8d6885bafda0d544da87634705
Hato <fansintuesan@gmail.com>
2024-07-20 22:55:00 +0800

    feat: side bar avatars

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,11 +4,12 @@ import { LightComponent } from "@app/common/three-model/light/light.component";
 import { FloorComponent } from "@app/common/three-model/floor/floor.component";
 import { ModelComponent } from "@app/common/three-model/model/model.component";
 import { StarsComponent } from "../common/three-model/stars/stars.component";
+import { AvatarComponent } from "../common/avatar/avatar.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CanvasComponent, LightComponent, FloorComponent, ModelComponent, StarsComponent],
+  imports: [CanvasComponent, LightComponent, FloorComponent, ModelComponent, StarsComponent, AvatarComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
