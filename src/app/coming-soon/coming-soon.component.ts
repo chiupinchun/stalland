fdf28c71c5af34032f6d815cce4b39a4ef838a44
Hato <fansintuesan@gmail.com>
2024-07-24 16:07:51 +0800

    feat: coming soon page add handle back

--- src/app/coming-soon/coming-soon.component.ts
+++ src/app/coming-soon/coming-soon.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { CanvasComponent } from "app/common/three-model/canvas/canvas.component";
 import { StarsComponent } from "app/common/three-model/stars/stars.component";
 import { LightComponent } from "app/common/three-model/light/light.component";
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-coming-soon',
@@ -15,9 +16,18 @@ export class ComingSoonComponent {
   count = 0
   countTimer?: ReturnType<typeof setInterval>
 
+  constructor(
+    @Inject(Location)
+    private readonly location: Location
+  ) { }
+
   getBackgroundSize() {
     const rate = 100 / Math.pow(2, this.count % 3)
     return `${rate}% ${rate}%`
+  }
+
+  handleBack() {
+    this.location.back()
   }
 
   ngAfterViewInit(): void {
